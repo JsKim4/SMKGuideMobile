0493bf7a69def257ead26b5f583d826f65f6683d
kjs <46150254+JsKim4@users.noreply.github.com>
2019-11-04 22:05:24 +0900

    TobaccoListView modify 19-11-04 22:04
    tobaccoFragment의 tobaccoList를 처리하기 위해 TobaccoListViewAdapter 추가 및 TobaccoVO추가 - 검색 설정을 위한 Criteria 추가

--- app/src/main/java/com/example/smkguide/ui/tobaccolist/TobaccoListFragment.java
+++ app/src/main/java/com/example/smkguide/ui/tobaccolist/TobaccoListFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.smkguide.ListViewAdapter.TobaccoListViewAdapter;
 import com.example.smkguide.R;
 
 public class TobaccoListFragment extends Fragment {
@@ -17,11 +19,14 @@ public class TobaccoListFragment extends Fragment {
         return  new TobaccoListFragment();
     }
 
-
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_tobaccolist, container, false);
+
+        ListView tobaccoListView = (ListView)root.findViewById(R.id.listTobacco);
+        TobaccoListViewAdapter adapter = new TobaccoListViewAdapter();
+        tobaccoListView.setAdapter(adapter);
+
         return root;
     }
 }
