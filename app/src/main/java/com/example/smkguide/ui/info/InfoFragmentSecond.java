c29511dacb889f0a42af3b6a13ce65bb9702767b
LeeGeunSu <gs.1226@daum.net>
2019-11-06 02:49:02 +0900

    Info (191106 0250)
    미완성

---
+++ app/src/main/java/com/example/smkguide/ui/info/InfoFragmentSecond.java
@@ -0,0 +1,32 @@
+package com.example.smkguide.ui.info;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.smkguide.R;
+
+public class InfoFragmentSecond extends Fragment {
+    public static InfoFragmentSecond newInstance(){
+        return new InfoFragmentSecond();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_info_second,
+                container, false);
+        return layout;
+    }
+}
---
+++ app/src/main/java/com/example/smkguide/ui/info/InfoFragmentThird.java
@@ -0,0 +1,32 @@
+package com.example.smkguide.ui.info;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.smkguide.R;
+
+public class InfoFragmentThird extends Fragment {
+    public static InfoFragmentThird newInstance(){
+        return new InfoFragmentThird();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_info_third,
+                container, false);
+        return layout;
+    }
+}
