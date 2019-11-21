01c5ef6f4a471dfbb0061b02ff2405dc6e9b8b60
LeeGeunSu <gs.1226@daum.net>
2019-11-22 03:41:04 +0900

    191122 0340 Info 폼 최종
    데이터 파싱만 하면 됨
    
    모든 스크롤바 다 숨기능 기능 추가
    스크롤바 버튼 크기 등 디자인 변경
    
    설명용 개발자 게시판 추가

--- app/src/main/java/com/example/smkguide/ui/info/InfoFragment.java
+++ app/src/main/java/com/example/smkguide/ui/info/InfoFragment.java
@@ -1,7 +1,5 @@
 package com.example.smkguide.ui.info;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +12,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.smkguide.R;
-import com.example.smkguide.domain.InfoVO;
 
 public class InfoFragment extends Fragment {
 
@@ -35,15 +32,19 @@ public class InfoFragment extends Fragment {
         Button btn_first = (Button) view.findViewById(R.id.btn_first);
         Button btn_second = (Button) view.findViewById(R.id.btn_second);
         Button btn_third = (Button) view.findViewById(R.id.btn_third);
+        Button btn_fourth = (Button) view.findViewById(R.id.btn_fourth);
         TextView textView1_2 = (TextView) view.findViewById(R.id.textView1_2);
         TextView textView1_3 = (TextView) view.findViewById(R.id.textView1_3);
         TextView textView1_4 = (TextView) view.findViewById(R.id.textView1_4);
+        TextView textView1_5 = (TextView) view.findViewById(R.id.textView1_5);
         TextView textView2_2 = (TextView) view.findViewById(R.id.textView2_2);
         TextView textView2_3 = (TextView) view.findViewById(R.id.textView2_3);
         TextView textView2_4 = (TextView) view.findViewById(R.id.textView2_4);
+        TextView textView2_5 = (TextView) view.findViewById(R.id.textView2_5);
         TextView textView3_2 = (TextView) view.findViewById(R.id.textView3_2);
         TextView textView3_3 = (TextView) view.findViewById(R.id.textView3_3);
         TextView textView3_4 = (TextView) view.findViewById(R.id.textView3_4);
+        TextView textView3_5 = (TextView) view.findViewById(R.id.textView3_5);
         TextView textView4_2 = (TextView) view.findViewById(R.id.textView4_2);
 
         View.OnClickListener listener = new View.OnClickListener()
@@ -64,6 +65,10 @@ public class InfoFragment extends Fragment {
                         fg = InfoFragmentThird.newInstance();
                         setFragment(fg);
                         break;
+                    case R.id.btn_fourth:
+                        fg = InfoFragmentFourth.newInstance();
+                        setFragment(fg);
+                        break;
                 }
             }
         };
@@ -71,69 +76,114 @@ public class InfoFragment extends Fragment {
         btn_first.setOnClickListener(listener);
         btn_second.setOnClickListener(listener);
         btn_third.setOnClickListener(listener);
+        btn_fourth.setOnClickListener(listener);
 
-        // 파싱시 데이터 연결시켜야됨
         View.OnClickListener dialog = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String title, content, name, date;
                 InfoDialog customDialog = new InfoDialog(getContext());
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                 switch(view.getId()) {
                     case R.id.textView1_2:
-                        //builder.setTitle("공지사항 1").setMessage("1-111111111111111111111111111111111111111111111111");
-                        //title=info.getTitle();
-                        //content=info.getContent();
-                        //name=info.getName();
-                        //date=info.getDate();
-                        //customDialog.call(title, content, name, date);
+                        title="공지사항 1";    // 파싱 데이터 넣어야됨
+                        content="내용1\n11111\n1\n111\n1\n11111";
+                        name="관리자A";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView1_3:
-                        builder.setTitle("공지사항 2").setMessage("1-2");
+                        title="공지사항 22222222";
+                        content="내용22222\n2222\n22\n2\n2\n2\n2\n2\n222\n222\n2\n222\n222\n2\n222\n222\n2\n222\n222\n2\n222\n222";
+                        name="관리자BBBBB";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView1_4:
-                        builder.setTitle("공지사항 3").setMessage("1-3");
+                        title="공지사항 333333333333333333333333333333333333333333";
+                        content="내용3\n33\n3\n3\n3\n3\n3\n3333333\n3\n3\n3333\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n33333333n3\n3\n3\n3\n3\n3\n3\n3\n3\n33333333";
+                        name="관리자C";
+                        date="2000-01-01 AM 03:30";
+                        break;
+                    case R.id.textView1_5:
+                        title="공지사항 4444444444444444444";
+                        content="내용4\n44\n4\n4\n4\n44444\n4\n344443\n4444444\n4\n4\n444\n4\n4\n4\n44\n44\n4444444";
+                        name="관리자C";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView2_2:
-                        builder.setTitle("뉴스 1").setMessage("2-1");
+                        title="뉴스 1";
+                        content="내용1\n11111\n1\n111\n1\n11111";
+                        name="관리자A";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView2_3:
-                        builder.setTitle("뉴스 2").setMessage("2-2");
+                        title="뉴스 2";
+                        content="내용22222\n2222\n22\n2\n2\n2\n2\n2\n2222222\n2";
+                        name="관리자BBBBB";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView2_4:
-                        builder.setTitle("뉴스 3").setMessage("2-3");
+                        title="뉴스 3333333333333333333333333333333333333333333";
+                        content="내용3\n33\n3\n3\n3\n3\n3\n3333333\n3\n3\n3333\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n33333333";
+                        name="관리자C";
+                        date="2000-01-01 AM 03:30";
+                        break;
+                    case R.id.textView2_5:
+                        title="뉴스 4444444444444444444";
+                        content="내용4\n44\n4\n4\n4\n44444\n4\n344443\n4444444\n4\n4\n444\n4\n4\n4\n44\n44\n4444444";
+                        name="관리자C";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView3_2:
-                        builder.setTitle("게시글 1").setMessage("3-1");
+                        title="게시판 1";
+                        content="내용1\n11111\n1\n111\n1\n11111";
+                        name="관리자A";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView3_3:
-                        builder.setTitle("게시글 2").setMessage("3-2");
+                        title="게시판 2";
+                        content="내용22222\n2222\n22\n2\n2\n2\n2\n2\n22222222";
+                        name="관리자BBBBB";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView3_4:
-                        builder.setTitle("게시글 3").setMessage("3-3");
+                        title="게시판 3333333333333333333333333333333333333333";
+                        content="내용3\n33\n3\n3\n3\n3\n3\n3333333\n3\n3\n3333\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n3\n33333333";
+                        name="관리자C";
+                        date="2000-01-01 AM 03:30";
+                        break;
+                    case R.id.textView3_5:
+                        title="게시판 4444444444444444444";
+                        content="내용4\n44\n4\n4\n4\n44444\n4\n344443\n4444444\n4\n4\n444\n4\n4\n4\n44\n44\n4444444";
+                        name="관리자C";
+                        date="2000-01-01 AM 03:30";
                         break;
                     case R.id.textView4_2:
-                        builder.setTitle("명지전문대학 전자공학과").setMessage("2015041001 김준섭\n2015041010 이근수\n2015041050 박성종\n2011041068 이승혁");
+                        title="명지전문대 전자공학과";
+                        content="2015041001 김준섭\n2015041010 이근수\n2015041050 박성종\n2011041068 이승혁";
+                        name="Admin";
+                        date="2000-01-01 AM 03:30";
                         break;
+                    default:
+                        title="";
+                        content="";
+                        name="";
+                        date="";
                 }
-                builder.setPositiveButton("닫기", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) { }
-                });
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+                customDialog.call(title, content, name, date);
             }
         };
 
         textView1_2.setOnClickListener(dialog);
         textView1_3.setOnClickListener(dialog);
         textView1_4.setOnClickListener(dialog);
+        textView1_5.setOnClickListener(dialog);
         textView2_2.setOnClickListener(dialog);
         textView2_3.setOnClickListener(dialog);
         textView2_4.setOnClickListener(dialog);
+        textView2_5.setOnClickListener(dialog);
         textView3_2.setOnClickListener(dialog);
         textView3_3.setOnClickListener(dialog);
         textView3_4.setOnClickListener(dialog);
+        textView3_5.setOnClickListener(dialog);
         textView4_2.setOnClickListener(dialog);
 
         return view;
--- app/src/main/java/com/example/smkguide/ui/info/InfoFragmentSecond.java
+++ app/src/main/java/com/example/smkguide/ui/info/InfoFragmentSecond.java
@@ -1,7 +1,5 @@
 package com.example.smkguide.ui.info;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
