009108f076af892e9cbd31c448aeff382f09be8d
kjs <46150254+JsKim4@users.noreply.github.com>
2019-12-01 16:14:54 +0900

    add Login and Comment write

--- app/src/main/java/com/example/smkguide/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/smkguide/ui/home/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.smkguide.ui.home;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.smkguide.R;
+import com.example.smkguide.task.member.LoginTask;
 
 public class HomeFragment extends Fragment {
 
@@ -21,6 +24,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
         return root;
     }
 }
